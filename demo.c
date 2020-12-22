ae2613b87267d590db1f4baf5305a9799052187c
AARONJVC <avanclea@cs.uoregon.edu>
2020-12-21 20:32:37 -0800

    interactive demo menu works

--- demo.c
+++ demo.c
@@ -25,6 +25,7 @@ Date Modified: 21 Dec 2020
 
 #define MAX_STR_LEN 256
 #define QUIT_COMMAND 'q'
+#define HELP_COMMAND 'h'
 
 //---------------------------------GLOBALS
 
@@ -53,7 +54,9 @@ int validate_int(char * s, int * i);
 // Validates a char from user
 int validate_char(char * s, char * c);
 
-//----------Y FUNCTIONS
+//----------DISPLAY FUNCTIONS
+
+void show_list();
 
 //----------Z FUNCTIONS
 
@@ -66,26 +69,16 @@ int main(int argc, char * argv[])
 	// Seeding pseudorandom generation
 	srand(time(0));
 
+	char user_command;
+
 	center_print("WELCOME", 24);
 
-	char user_command;
+	show_list();
 
 	do
 	{
-		int function_id = 0;
-
-		for(int i = 0; g_all[i] != NULL; ++i)
-		{
-			for(int j = 0; g_all[i][j] != NULL; ++j)
-			{
-				printf("%2i - %s\n", function_id, g_all[i][j]);
-
-				++function_id;
-			}
-		}
-
-		printf("\nTo test a function, enter the number by its name.\n");
-		printf("Enter \"q\" to quit.\n");
+		printf("\nTo test a function, enter its listed number.\n");
+		printf("Enter a number, \"%c\" to see the list again, or \"%c\" to quit.\n", HELP_COMMAND, QUIT_COMMAND);
 		printf(">>> ");
 
 		char * buffer = NULL;
@@ -96,144 +89,43 @@ int main(int argc, char * argv[])
 		{
 			if(!get_input(&buffer))
 			{
-				printf("get input succeeded\n");
-
 				if(!validate_char(buffer, &user_command))
 				{
 					invalid = 0;
 				}
-
-				printf("%s\n", buffer);
-
 				free(buffer);
-
-				printf("buffer freed\n");
 			}
 		}
 
-		printf("%c\n", user_command);
-
-		//user_command = QUIT_COMMAND;
+		switch(user_command)
+		{
+			case QUIT_COMMAND:
+			{
+				break;
+			}
+			case HELP_COMMAND:
+			{
+				printf("\n");
+				show_list();
+				break;
+			}
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			{
+				printf("\nFunction %c selected\n", user_command);
+				break;
+			}
+			default:
+			{
+				printf("\nInvalid command\n");
+				break;
+			}
+		}
 	}
 	while(user_command != QUIT_COMMAND);
-
-	/*
-	center_print("WELCOME", 24);
-
-	timed_print("I'm a fast print\n", 2);
-
-	//timed_print("I'm a slow print\n", 10);
-
-
-	timed_print("", 2);
-
-	timed_print("\n", 2);
-
-	timed_print("A\n", 2);
-
-	timed_print("AB\n", 2);
-
-	timed_print("ABC\n", 2);
-
-	timed_print("ABCD\n", 2);
-
-	timed_print("ABCDE\n", 2);
-
-	timed_print("ABCDEF\n", 2);
-
-	timed_print("ABCDEFG\n", 2);
-
-	timed_print("ABCDEFGH\n", 2);
-
-	timed_print("ABCDEFGHI\n", 2);
-
-	timed_print("ABCDEFGHIJ\n", 2);
-
-	timed_print("ABCDEFGHIJK\n", 2);
-
-	timed_print("ABCDEFGHIJKL\n", 2);
-
-	timed_print("ABCDEFGHIJKLM\n", 2);
-
-	timed_print("ABCDEFGHIJKLMN\n", 2);
-
-	timed_print("", 0.0002);
-
-	timed_print("\n", 0.0002);
-
-	timed_print("A\n", 0.0002);
-
-	timed_print("AB\n", 0.0002);
-
-	timed_print("ABC\n", 0.0002);
-
-	timed_print("ABCD\n", 0.0002);
-
-	timed_print("ABCDE\n", 0.0002);
-
-	timed_print("ABCDEF\n", 0.0002);
-
-	timed_print("ABCDEFG\n", 0.0002);
-
-	timed_print("ABCDEFGH\n", 0.0002);
-
-	timed_print("ABCDEFGHI\n", 0.0002);
-
-	timed_print("ABCDEFGHIJ\n", 0.0002);
-
-	timed_print("ABCDEFGHIJK\n", 0.0002);
-
-	timed_print("ABCDEFGHIJKL\n", 0.0002);
-
-	timed_print("ABCDEFGHIJKLM\n", 0.0002);
-
-	timed_print("ABCDEFGHIJKLMN\n", 0.0002);
-
-
-	timed_print("A long message printed in a short amount of time\n", 0.5);
-
-	timed_print("An even longer message printed in a disproportionately longer amount of time\n", 5);
-
-	sleepy_print("I'm sleepy\n", 0.1);
-
-	scream_print("ouch", 0.25, 0.05, 10, 10);
-
-	printf("\n");
-
-	scream_print("a", 0.1, 0.01, 64, 128);
-
-	printf("\n");
-
-	scream_print("h", 0.1, 0.01, 64, 32);
-
-	printf("\n");
-
-	scream_print("a", 0.1, 0.01, 0, 32);
-
-	printf("\n");
-
-	scream_print("a", 0.1, 0.01, 32, 0);
-
-	printf("\n");
-
-	scream_print("a", 0.1, 0.3, 0, 32);
-
-	printf("\n");
-
-	scream_print("a", 0.1, 0.3, 32, 0);
-
-	printf("\n");
-
-	scream_print("a", 0.1, 0, 32, 0);
-
-	accel_print("This line of text will print faster and faster over time\n", 0.1, 0.01);
-
-	accel_print("This line of text will print slower and slower over time\n", 0.1, 0.5);
-
-	accel_print("This line of text will print at a constant speed\n", 0.1, 0.1);
-
-	accel_print("This line of text will print faster and faster over time\n", 0.1, 0.0001);
-	*/
 
 	return EXIT_SUCCESS;
 }
@@ -252,25 +144,16 @@ int get_input(char ** buffer)
 
 	if(getline(&temp_buffer, &temp_size, stdin) != -1)
 	{
-		printf("Got line\n");
-
 		if((*buffer = (char *)malloc(MAX_STR_LEN * sizeof(char))) != NULL)
 		{
-			printf("allocated buffer\n");
-
 			strncpy(*buffer, temp_buffer, (MAX_STR_LEN - 1));
-
-			printf("copied to buffer\n");
 
 			(*buffer)[MAX_STR_LEN - 1] = '\0';
 
 			status = 0;
 		}
 	}
-
 	free(temp_buffer);
-
-	printf("freed line\n");
 
 	return status;
 }
@@ -336,7 +219,22 @@ int validate_char(char * s, char * c)
 	return 0;
 }
 
-//----------Y FUNCTIONS
+//----------DISPLAY FUNCTIONS
+
+void show_list()
+{
+	int function_id = 0;
+
+	for(int i = 0; g_all[i] != NULL; ++i)
+	{
+		for(int j = 0; g_all[i][j] != NULL; ++j)
+		{
+			printf("%2i - %s\n", function_id, g_all[i][j]);
+
+			++function_id;
+		}
+	}
+}
 
 //----------Z FUNCTIONS
 
